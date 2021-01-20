2b717c51f16a07d740a63d962bb6e237b9200616
blaj <36105866+blaj@users.noreply.github.com>
2021-01-20 19:23:39 +0100

    Feature/print test in console (#2)
    * feat(app):
    
    * feat(app): display test in console [#1]
    
    closes #1

---
+++ TestowaApka2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestowaApka2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Out.WriteLine("test");
+            Console.Out.WriteLine("test");
+        }
+    }
+}
